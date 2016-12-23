f82769c1cf5bc170ccf6950c6700f21a0e37353c
pinguindark <darkpinguino@gmail.cpm>
2016-12-22 23:38:11 -0300

    fix heroku app config

--- config/heroku_app.php
+++ config/heroku_app.php
@@ -220,16 +220,16 @@ return [
             'className' => 'Cake\Database\Connection',
             'driver' => 'Cake\Database\Driver\Mysql',
             'persistent' => false,
-            'host' => 'us-cdbr-iron-east-04.cleardb.net',
+            'host' => '[us-cdbr-iron-east-04.cleardb.net]',
             /**
              * CakePHP will use the default DB port based on the driver selected
              * MySQL on MAMP uses port 8889, MAMP users will want to uncomment
              * the following line and set the port accordingly
              */
             //'port' => 'non_standard_port_number',
-            'username' => 'b6d8710f51364c',
-            'password' => 'b1bac4f0',
-            'database' => 'heroku_694df273540ff7d',
+            'username' => '[b6d8710f51364c]',
+            'password' => '[b1bac4f0]',
+            'database' => '[heroku_694df273540ff7d]',
             'encoding' => 'utf8',
             'timezone' => 'UTC',
             'flags' => [],
