5ba488bbb403ab41dfdaa2b012139f4c40017aa0
pinguindark <darkpinguino@gmail.cpm>
2016-12-23 00:39:42 -0300

    fix heroku app config

--- config/heroku_app.php
+++ config/heroku_app.php
@@ -1,6 +1,6 @@
 <?php
 
-// $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$db = parse_url(env('CLEARDB_DATABASE_URL'));
 return [
     /**
      * Debug Level:
@@ -217,49 +217,65 @@ return [
      */
 
     // mysql://b6d8710f51364c:b1bac4f0@us-cdbr-iron-east-04.cleardb.net/heroku_694df273540ff7d?reconnect=true
-    'Datasources' => [
+    // 'Datasources' => [
+    //     'default' => [
+    //         'className' => 'Cake\Database\Connection',
+    //         'driver' => 'Cake\Database\Driver\Mysql',
+    //         'persistent' => false,
+    //         'host' => 'us-cdbr-iron-east-04.cleardb.net',
+    //         /**
+    //          * CakePHP will use the default DB port based on the driver selected
+    //          * MySQL on MAMP uses port 8889, MAMP users will want to uncomment
+    //          * the following line and set the port accordingly
+    //          */
+    //         //'port' => 'non_standard_port_number',
+    //         'username' => 'DB_USERNAME', 'b6d8710f51364c',
+    //         'password' => 'DB_PASSWORD', 'b1bac4f0',
+    //         'database' => 'DB_DATABASE', 'heroku_694df273540ff7d',
+    //         // 'encoding' => 'utf8',
+    //         // 'timezone' => 'UTC',
+    //         // 'flags' => [],
+    //         // 'cacheMetadata' => true,
+    //         // 'log' => false,
+
+    //         *
+    //          * Set identifier quoting to true if you are using reserved words or
+    //          * special characters in your table or column names. Enabling this
+    //          * setting will result in queries built using the Query Builder having
+    //          * identifiers quoted when creating SQL. It should be noted that this
+    //          * decreases performance because each query needs to be traversed and
+    //          * manipulated before being executed.
+             
+    //         'quoteIdentifiers' => false,
+
+    //         /**
+    //          * During development, if using MySQL < 5.6, uncommenting the
+    //          * following line could boost the speed at which schema metadata is
+    //          * fetched from the database. It can also be set directly with the
+    //          * mysql configuration directive 'innodb_stats_on_metadata = 0'
+    //          * which is the recommended value in production environments
+    //          */
+    //         //'init' => ['SET GLOBAL innodb_stats_on_metadata = 0'],
+            
+    //         // 'url' => env('DATABASE_URL', null),
+    //         'url' => getenv('CLEARDB_DATABASE_URL', null),
+    //     ],
+
+        'Datasources' => [
         'default' => [
             'className' => 'Cake\Database\Connection',
             'driver' => 'Cake\Database\Driver\Mysql',
             'persistent' => false,
-            'host' => 'us-cdbr-iron-east-04.cleardb.net',
-            /**
-             * CakePHP will use the default DB port based on the driver selected
-             * MySQL on MAMP uses port 8889, MAMP users will want to uncomment
-             * the following line and set the port accordingly
-             */
-            //'port' => 'non_standard_port_number',
-            'username' => 'DB_USERNAME', 'b6d8710f51364c',
-            'password' => 'DB_PASSWORD', 'b1bac4f0',
-            'database' => 'DB_DATABASE', 'heroku_694df273540ff7d',
-            // 'encoding' => 'utf8',
-            // 'timezone' => 'UTC',
-            // 'flags' => [],
-            // 'cacheMetadata' => true,
-            // 'log' => false,
-
-            /**
-             * Set identifier quoting to true if you are using reserved words or
-             * special characters in your table or column names. Enabling this
-             * setting will result in queries built using the Query Builder having
-             * identifiers quoted when creating SQL. It should be noted that this
-             * decreases performance because each query needs to be traversed and
-             * manipulated before being executed.
-             */
+            'host' => $db['host'],
+            'username' => $db['user'],
+            'password' => $db['pass'],
+            'database' => substr($db['path'], 1),
+            'encoding' => 'utf8',
+            'timezone' => 'UTC',
+            'cacheMetadata' => true,
             'quoteIdentifiers' => false,
-
-            /**
-             * During development, if using MySQL < 5.6, uncommenting the
-             * following line could boost the speed at which schema metadata is
-             * fetched from the database. It can also be set directly with the
-             * mysql configuration directive 'innodb_stats_on_metadata = 0'
-             * which is the recommended value in production environments
-             */
-            //'init' => ['SET GLOBAL innodb_stats_on_metadata = 0'],
-            
-            // 'url' => env('DATABASE_URL', null),
-            'url' => getenv('CLEARDB_DATABASE_URL', null),
         ],
+    ],
 
         /**
          * The test connection is used during the test suite.
