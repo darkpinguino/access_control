4e38e77310aa32421917baa7bf089c8b4075155e
pinguindark <darkpinguino@gmail.cpm>
2016-12-28 20:20:53 -0300

    add app.default
    copia del conf/app.php para heroku

--- config/app.default2.php
+++ config/app.default2.php
@@ -1,7 +1,4 @@
 <?php
-
-$db = parse_url(env('CLEARDB_DATABASE_URL'));
-
 return [
     /**
      * Debug Level:
@@ -66,7 +63,7 @@ return [
      *   You should treat it as extremely sensitive data.
      */
     'Security' => [
-        'salt' => env('SECURITY_SALT', '30814AJSDJADde0f7f9d6f1f5a37JASLJDce820bfa6c9273954f3f17394f93f0db5893899cade'),
+        'salt' => env('SECURITY_SALT', '__SALT__'),
     ],
 
     /**
@@ -221,14 +218,42 @@ return [
             'className' => 'Cake\Database\Connection',
             'driver' => 'Cake\Database\Driver\Mysql',
             'persistent' => false,
-            'host' => $db['host'],
-            'username' => $db['user'],
-            'password' => $db['pass'],
-            'database' => substr($db['path'], 1),
+            'host' => 'localhost',
+            /**
+             * CakePHP will use the default DB port based on the driver selected
+             * MySQL on MAMP uses port 8889, MAMP users will want to uncomment
+             * the following line and set the port accordingly
+             */
+            //'port' => 'non_standard_port_number',
+            'username' => 'my_app',
+            'password' => 'secret',
+            'database' => 'my_app',
             'encoding' => 'utf8',
             'timezone' => 'UTC',
+            'flags' => [],
             'cacheMetadata' => true,
+            'log' => false,
+
+            /**
+             * Set identifier quoting to true if you are using reserved words or
+             * special characters in your table or column names. Enabling this
+             * setting will result in queries built using the Query Builder having
+             * identifiers quoted when creating SQL. It should be noted that this
+             * decreases performance because each query needs to be traversed and
+             * manipulated before being executed.
+             */
             'quoteIdentifiers' => false,
+
+            /**
+             * During development, if using MySQL < 5.6, uncommenting the
+             * following line could boost the speed at which schema metadata is
+             * fetched from the database. It can also be set directly with the
+             * mysql configuration directive 'innodb_stats_on_metadata = 0'
+             * which is the recommended value in production environments
+             */
+            //'init' => ['SET GLOBAL innodb_stats_on_metadata = 0'],
+            
+            'url' => env('DATABASE_URL', null),
         ],
 
         /**
